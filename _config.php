efe2a5e69115a05c265fae5df72d158aad93d4ee
Morven Lewis-Everley <morven@i-lateral.com>
2023-10-26 17:17:53 +0100

    Add basic shortcode handler

--- _config.php
+++ _config.php
@@ -1,1 +1,12 @@
 <?php
+
+use ilateral\SilverStripe\Gallery\ShortCodes\GalleryShortCodeHandler;
+use SilverStripe\View\Parsers\ShortcodeParser;
+
+ShortcodeParser::get('default')->register(
+    GalleryShortCodeHandler::SHORTCODE_GALLERY,
+    [
+        GalleryShortCodeHandler::class,
+        'handle_shortcode'
+    ]
+);
---
+++ src/ShortCodes/GalleryShortCodeHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ilateral\SilverStripe\Gallery\ShortCodes;
+
+use SilverStripe\View\Parsers\ShortcodeParser;
+use SilverStripe\View\Parsers\ShortcodeHandler;
+use ilateral\SilverStripe\Gallery\Model\Gallery;
+
+/**
+ * Allow embeding shortcodes in a content area.
+ * Currently this shortcode supports the format:
+ * 
+ * [gallery,code="gallery-code"]
+ */
+class GalleryShortCodeHandler implements ShortcodeHandler
+{
+    const SHORTCODE_GALLERY = 'gallery';
+
+	public static function get_shortcodes(): array
+	{
+		return [self::SHORTCODE_GALLERY];
+	}
+
+	/**
+	 *
+	 * @param array $arguments
+	 * @param string $content
+	 * @param ShortcodeParser $parser
+	 * @param string $shortcode
+	 * @param array $extra
+	 *
+	 * @return string
+	 */
+	public static function handle_shortcode(
+        $arguments,
+        $content,
+        $parser,
+        $shortcode,
+        $extra = array()
+    ) {
+        // Ensure we are calling a specific gallery
+        if (empty($arguments['code'])) {
+            return;
+        }
+
+        $code = $arguments['code'];
+        /** @var Gallery */
+        $gallery = Gallery::get()->find('Code', $code);
+
+        if (empty($gallery)) {
+            return;
+        }
+
+        return $gallery->forTemplate();
+    }
+}
